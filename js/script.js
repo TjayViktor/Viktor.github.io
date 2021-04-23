7b7e9d94beffa7732252f9b18df64cd46c0c8878
ViktorDevelopment <Artemevskayasvetlana@mail.ru>
2021-04-24 01:50:18 +0300

    jq

--- js/script.js
+++ js/script.js
@@ -60,4 +60,6 @@ $(document).ready(function(){
 
       validateForms('#consultation-form');
       validateForms('#consultation form');
+
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
   });
